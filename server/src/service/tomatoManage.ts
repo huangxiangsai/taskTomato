11459af63a2c661b90d3ac0ce425c9434c96eeb9
469181336@qq.com <469181336@qq.com>
2017-01-16 23:41:20 +0800

    add 添加获得番茄分页列表

--- server/src/service/tomatoManage.ts
+++ server/src/service/tomatoManage.ts
@@ -6,7 +6,7 @@ import { T_STATUS } from '../schemas/taskSchema';
 export class TomatoManage {
 
 	/**
-	 * 保存番茄
+	 * 保存番茄 , 包括 完成的，以及打断的
 	 */
 	create(tomato : Object){
 		return new Promise(function(resolve,reject){
@@ -34,9 +34,25 @@ export class TomatoManage {
 
 	}
 
-	//获得指定日期段完成的番茄
-	getTomatoByDate(){
-
+	/**
+	 * 获得指定日期段完成的番茄,并且分页
+	 * @param userId 用户ID
+	 * @param startTime 开始时间
+	 * @param endTime  结束时间
+	 * @param pageNumber 当前页数
+	 * @param size  每页个数
+	 */
+	getTomatoByDate(userId,startTime,endTime,pageNumber,size,status){
+		return new Promise(function(resolve,reject){
+			let query = Tomato.find({userId : userId, status:status , startDate:  { $gte: startTime, $lt: endTime  } });
+			let promise = query.sort('startDate').skip((pageNumber-1)*size).limit(size);
+			promise.then(function(tomatos){
+				let total = query.count();
+				resolve({coe : 200 , data : {total : total, pageNumber : pageNumber , size : size , data : tomatos}});
+			},function(err){
+				reject({code : -1 , msg : '获得番茄列表失败'});
+			});
+		});
 	}
 
 	/**
@@ -45,10 +61,10 @@ export class TomatoManage {
 	getTomatoToday(userId,status){
 		let date = new Date();
 		let startTime = date.getTime() - (date.getHours() * 3600000 + date.getMinutes() * 60000 + date.getSeconds() * 1000);
-		let endtTime = startTime + 24*3600000;
+		let endTime = startTime + 24*3600000;
 		
 		return new Promise(function(resolve,reject){
-			let promise = Tomato.find({userId : userId, status : status ,  startDate:  { $gte: startTime, $lt: endtTime  } }).exec();
+			let promise = Tomato.find({userId : userId, status : status ,  startDate:  { $gte: startTime, $lt: endTime  } }).exec();
 			promise.then(function(tomatos){
 				resolve({code : 200 , data : tomatos , msg : 'tomato list'});
 			}, function(err){
@@ -57,6 +73,8 @@ export class TomatoManage {
 
 		});
 	}
+
+
 	
 
 }
